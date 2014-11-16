85703b0e26498d94ff4c440f14e75b7c679a8edb
Arielle <arielle.bryn@gmail.com>
2014-11-16 05:02:31 -0500

    oops and this

--- UnityStuff/Assets/Scripts/RockSelector.js
+++ UnityStuff/Assets/Scripts/RockSelector.js
@@ -4,7 +4,6 @@ function Start () {
 	var n : int = Random.Range(1, 20);
 	var mesh : Mesh;
 	var children : Component[] = this.GetComponentsInChildren(Transform, false);
-	print(children.Length);
 	for (var i = 1; i < children.Length; i++) {
 		if (i == n) {
 		} else {
@@ -18,7 +17,6 @@ function Update () {
 }
 
 function OnCollisionEnter (col : Collision) {
-	print(col.relativeVelocity.magnitude);
 	if (col.relativeVelocity.magnitude > 10) {
 			Destroy(this);
 	}
